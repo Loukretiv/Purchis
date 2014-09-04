16bd35baf4a90fda8dcab8b7523299d0b2049e16
Joram Clervius <clervius@loukretiv.com>
2014-09-04 07:46:55 -0400

    Signup Page beginnings creation and typography fix
    Typography: Chrome's new update prevents the loading of Segoe UI Light
    fonts, this makes it necessary to ensure that all the fallback fonts are
    working properly. We have Proxima Nova from Adobe TypeKit As the main
    fallback font. It is set to work on http://localhost/ as well as
    Purchis.com so you can test it out locally.
    
    Sign Up: Created the beginnings of the Sign up form process. There is
    still the main form and agreement to implement.

--- js/login.js
+++ js/login.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	$(document).on("click",".switching", function(){
 		var container = $(this).parent('div');
 		var other = container.siblings('div');
-		console.log(other.html())
 		container.fadeOut("fast", function(){
 			other.fadeIn();
 			})
